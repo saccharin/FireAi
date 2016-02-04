52dc97783f464d9b9ae4a810290139acf5f110f4
saccharin <chriswaful@hotmail.com>
2016-02-04 15:49:15 -0500

    First path at the math
    Index will now draw the objects as they interact
    Added event-like methods
    Proof-of-concept demo now working

--- js/projectile.js
+++ js/projectile.js
@@ -1,31 +1,37 @@
 var Projectile = function(initialP, initialV) {
-	this.currentP = initialP;
-	this.initialV = initialV;
+	this.currentP = JSON.parse(JSON.stringify(initialP));
+	this.initialV = JSON.parse(JSON.stringify(initialV));
 	
 	this.currentV = JSON.parse(JSON.stringify(initialV));
 	
 	
-	this.mass = 1;
+	this.mass = 10;
 	this.iteration = 0;
 	
 	this.isAlive = true;
 }
 
 Projectile.prototype.getNextV = function(actors) {
-	var x = this.currentV.x;
-	var y = this.currentV.y;
+	var p = this;
 	
 	actors.forEach(function(a) {
-		a.attract(this);
-		a.fire(this);
+		a.attract(p);
+		a.fire(p);
 	});
 	
 	if(!this.isAlive)
 		return;
 	
+	this.currentP.x += this.currentV.x;
+	this.currentP.y += this.currentV.y;
 	
+	this.onMove(this);
 }
 
 Projectile.prototype.kill = function() {
 	this.isAlive = false;
+	this.onDie(this);
 }
+
+Projectile.prototype.onMove = function() {}
+Projectile.prototype.onDie = function() {}
